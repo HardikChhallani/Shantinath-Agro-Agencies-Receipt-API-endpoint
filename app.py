7f7577d2cebdca56cca583442219b0d981696f11
HardikChhallani <hardikchhallani04@gmail.com>
2024-10-07 23:14:48 +0530

    initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from application import *
+from flask import Flask, request, send_file
+import pdfkit
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/generate-SAA-receipt-pdf', methods=['GET'])
+def return_pdf():
+    return generate_receipts()
+
+if __name__ == '__main__':
+    app.run(debug=True)
